014ac87fdd37d6e102b3a9d00f33c17c1832c776
Amit1332 <amitsinghpatel9747@gmail.om>
2023-09-26 19:31:43 +0530

    first

---
+++ src/Components/Buttons.js
@@ -0,0 +1,90 @@
+import React, {Component, useState} from 'react'
+
+export const Buttons = () => {
+
+    const [classCompoRender, setClassCompoRender] = useState(null);
+    const [functionCompoRender, setFunctionCompoRender] = useState(null);
+    const [flag, setFlag] = useState(1);
+    const [flag1, setFlag1] = useState(1);
+
+    function classbased() {
+        if(flag===1){
+            setClassCompoRender(<ClassBasedComp />)
+            setFlag(0);
+        }
+        else{
+            setClassCompoRender(null);
+            setFlag(1);
+        }
+    }
+
+
+    function functionbased() {
+        if(flag1===1){
+            setFunctionCompoRender(<FunctionBasedComp />);
+            setFlag1(0);
+        }
+        else{
+            setFunctionCompoRender(null);
+            setFlag1(1);
+        }
+        
+    }
+  return (
+    <>
+    
+    <div className="container">
+        <div className="left">
+        <button onClick={classbased}>Class Based Component</button>
+     {classCompoRender}
+
+
+        </div>
+
+        <div className="right">
+        <button onClick={functionbased}>Function Based Component</button>
+        {functionCompoRender}
+
+        </div>
+
+    </div>
+
+   
+    
+   
+    
+    </>
+  )
+}
+
+
+class ClassBasedComp extends Component {
+    render(){
+        return (
+            <>
+            <div className="card">
+                <h2>This is created using Class Component</h2>
+                <h6>This is done using external css</h6>
+                <h6 style={{color:"red"}}>This is done using internal css</h6>
+
+            </div>
+
+            </>
+        )
+    }
+}
+
+
+
+function FunctionBasedComp(){
+    return ( 
+        <>
+        
+        <div className="card" style={{backgroundColor:"purple",color:"white"}}>
+                <h2>This is created using Function Component</h2>
+                <h6>This is done using external css</h6>
+                <h6 style={{color:"green"}}>This is done using internal css</h6>
+            </div>
+        </>
+    )
+}
---
+++ src/Components/Hello.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Hello = () => {
+  return (
+    <h1 style={{marginTop:"50px"}}>Styling Using Class and Functional Component 👍</h1>
+  )
+}
